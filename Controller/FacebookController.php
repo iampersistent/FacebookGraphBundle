2ea26ac2a3e95bc9ab6a195d04581174e4823b7e
IamPersistent <develop@zestic.com>
2011-11-19 22:22:32 -0800

    add events

--- Controller/FacebookController.php
+++ Controller/FacebookController.php
@@ -7,15 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FacebookController extends Controller
 {
+    public function lastEventsAction($facebookId, $limit = 10, $age = null)
+    {
+        $graphApi = $this->get('iampersistent_facebook_graph_api');
+
+        $posts = $graphApi->fetchEvents($facebookId);
+
+        $response = $this->render('IamPersistentFacebookGraphBundle:Facebook:lastEvents.html.twig', array(
+            'facebookId' => $facebookId,
+            'posts' => $posts,
+        ));
+
+        if ($age) {
+            $response->setSharedMaxAge($age);
+        }
+
+        return $response;
+    }
+
     public function lastPostsAction($facebookId, $limit = 10, $age = null)
     {
         $graphApi = $this->get('iampersistent_facebook_graph_api');
 
-        $posts = $graphApi->fetchPosts($facebookId);
+        $events = $graphApi->fetchPosts($facebookId);
 
         $response = $this->render('IamPersistentFacebookGraphBundle:Facebook:lastPosts.html.twig', array(
             'facebookId' => $facebookId,
-            'posts' => $posts,
+            'events' => $events,
         ));
 
         if ($age) {
